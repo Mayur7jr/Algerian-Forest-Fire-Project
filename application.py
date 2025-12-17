8a64293fa96469e64cf511437ab2523c16144afa
Mayur7jr <deshpandemayureshp@gmail.com>
2025-12-17 22:18:01 +0530

    Initial commit: Algerian Forest Fire Prediction project

---
+++ application.py
@@ -0,0 +1,48 @@
+from flask import Flask,request,jsonify,render_template
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.preprocessing import StandardScaler
+
+
+application=Flask(__name__)
+app=application
+
+#import ridge regressor and standard scaler pickle
+ridge_model=pickle.load(open('models/RidgeRegression.pkl','rb'))
+standard_scaler=pickle.load(open('models/StandardScaler.pkl','rb'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='POST':
+        Temperature=float(request.form['Temperature'])
+        RH=float(request.form['RH'])
+        Ws=float(request.form['Ws'])
+        Rain=float(request.form['Rain'])
+        FFMC=float(request.form['FFMC'])
+        DMC=float(request.form['DMC'])
+        ISI=float(request.form['ISI'])
+        Classes=float(request.form['Classes'])
+        Region=float(request.form['Region'])
+
+        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data_scaled)
+
+        return render_template('home.html',results=result[0])
+
+
+    else:
+        return render_template('home.html')
+     
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
